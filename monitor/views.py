eb99a1aa48607ea2def1df850e06afcc62f7c7ad
jaugor7 <sudhansh0706@gmail.com>
2021-05-16 17:52:11 +0530

    Adding Correct Input Files

--- monitor/views.py
+++ monitor/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
 import csv
+import glob
+import pandas as pd
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
+
+def createAttendence():
+
+    path_csv=glob.glob("./AttendaceManagementSystem/Attendance/*.csv")
+    
+    df = pd.read_csv(path_csv[0], delimiter=',', encoding='UTF-8')
+    combined_csv_data = pd.concat([pd.read_csv(f, delimiter=',', encoding='UTF-8') for f in path_csv])
+    combined_csv_data.to_csv('Attendance.csv')
+
+
 def teacherView(request):
     students = []
     records = dict()
+
+    createAttendence()
 
     with open('Attendance.csv', 'r') as csvfile:
         csvreader = csv.reader(csvfile)
@@ -17,13 +31,13 @@ def teacherView(request):
 
         for line in csvreader:
 
-            name = line[1].strip("[']").split()
+            name = line[2].strip("[']").split()
             firstName = name[0]
             lastName = ""
             if len(name) > 1:
                 lastName = name[1]
 
-            rollNumber = line[0]
+            rollNumber = line[1]
 
             records[rollNumber] =  {
                 'firstName': firstName,
@@ -56,20 +70,19 @@ def studentView(request, rollNumber):
 
         for line in csvreader:
 
-            if line[0] != rollNumber:
+            if line[1] != rollNumber:
                 continue
 
-            print(line)
-
-            name = line[1].strip("[']").split()
+            name = line[2].strip("[']").split()
+            print(name)
 
             firstName = name[0]
             if len(name) > 1:
                 lastName = name[1]
 
             tempDict = {
-                'date': line[2],
-                'time': line[3],
+                'date': line[3],
+                'time': line[4],
             }
 
             studentRecords.append(tempDict)
